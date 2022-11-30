694fba5f02c6209f3a5b43565d227003f63525a8
luis782006 <luisfelipe78006@gmail.com>
2022-11-30 10:35:48 -0300

    timer de presentacion

--- Precios-Justos/src/app/componentes/home/home.component.ts
+++ Precios-Justos/src/app/componentes/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {interval,timer} from 'rxjs';
+import { Router } from '@angular/router';
+import {interval,take,takeUntil,timer} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,19 +8,43 @@ import {interval,timer} from 'rxjs';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  //https://puntotech.github.io/rxjs-docu/operators/filtering/takeUntil
+  //https://puntotech.github.io/rxjs-docu/operators/f iltering/takeUntil
+  //this.router.navigateByUrl('/login');
   
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
+
+  opaTitulo:any;
+  opaNacion:any;
+  opaDerechos:any;
+
 
   ngOnInit(): void {
-    const counting=interval(1000);
+    const counting = interval(1000);
+    const time=timer(5000);
+    const takeTime = counting.pipe(take(5));
+    takeTime.subscribe(x => {
+      switch (x) {
+        case x=0:
+          this.opaTitulo=1
+          break;
+        case x=1:
+          this.opaNacion=1
+          break;
+        case x=2:
+          this.opaDerechos=1
+          break;
+          case x=4:
+            this.router.navigateByUrl('/miruta');
+            break;
+        default:
+          break;
+      }  
+    });
 
-    counting.subscribe((sec)=>{
-      if (sec>1) {
-        console.log(sec);
-        
-      }
-    })
-  }
 
+}
+ 
+   
 }
