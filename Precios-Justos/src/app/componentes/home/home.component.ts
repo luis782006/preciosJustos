62b1af16a5d49a545b4e13b2e0e0137307c68cac
luis782006 <luisfelipe782006@gmail.com>
2022-11-30 07:07:07 -0300

    pagina de inicio

--- Precios-Justos/src/app/componentes/home/home.component.ts
+++ Precios-Justos/src/app/componentes/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {interval,timer} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-
+  //https://puntotech.github.io/rxjs-docu/operators/filtering/takeUntil
+  
   constructor() { }
 
   ngOnInit(): void {
-   
+    const counting=interval(1000);
+
+    counting.subscribe((sec)=>{
+      if (sec>1) {
+        console.log(sec);
+        
+      }
+    })
   }
 
 }
